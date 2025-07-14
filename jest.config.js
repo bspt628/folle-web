55ca3d4629c2db1e8d4f2f693d6235914c94771c
bspt628 <hirotobspt628@gmail.com>
2025-07-14 15:07:34 +0900

    fix

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,22 @@
-module.exports = {
-	testEnvironment: "jest-environment-jsdom",
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+	// next.config.jsとテスト環境用の.envファイルが配置されたディレクトリをセット
+	dir: "./",
+});
+
+// Jest のカスタム設定を設置する場所
+const customJestConfig = {
 	setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+	moduleDirectories: ["node_modules", "<rootDir>/"],
+	testEnvironment: "jest-environment-jsdom",
 	moduleNameMapper: {
 		"^@/(.*)$": "<rootDir>/$1",
-		"\\.(css|less|sass|scss)$": "identity-obj-proxy",
 	},
-	testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
 	transform: {
-		"^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+		"^.+\\.(js|jsx|ts|tsx)$": ["@swc/jest"],
 	},
-	moduleDirectories: ["node_modules", "<rootDir>"],
 };
+
+// createJestConfigを定義することによって、本ファイルで定義された設定がNext.jsの設定に反映されます
+module.exports = createJestConfig(customJestConfig);
